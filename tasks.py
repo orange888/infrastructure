8bd06abaff4444ba4673b7bfed60279d3238ed44
Jesse B. Hannah <jesse@jbhannah.net>
2019-09-01 14:22:58 -0700

    Invoke tasks script

---
+++ tasks.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+
+from invoke import Collection, task
+
+BASE_CMD = ["pipenv", "run", "ansible-playbook"]
+
+DEFAULT_ENV = {"OBJC_DISABLE_INITIALIZE_FORK_SAFETY": "YES"}
+TASK_DEFAULT_ENVS = {
+    "bootstrap": {
+        "ANSIBLE_ASK_PASS": "true",
+        "ANSIBLE_HOST_KEY_CHECKING": "false"
+    }
+}
+
+ENV_ANSIBLE_REMOTE_USER = "ANSIBLE_REMOTE_USER"
+
+
+def playbook_task_factory(name):
+    def _fn(c, limit=None, tags=None, user=None):
+        cmd = BASE_CMD.copy()
+
+        if limit is not None:
+            cmd.append("--limit={}".format(limit))
+
+        if tags is not None:
+            cmd.append("--tags={}".format(tags))
+
+        if user is not None:
+            cmd.append("--extra-vars=\"ansible_user={}\"".format(user))
+
+        env = DEFAULT_ENV.copy()
+
+        if name in TASK_DEFAULT_ENVS:
+            env.update(TASK_DEFAULT_ENVS[name])
+
+        c.run(" ".join([*cmd, "ansible/{}.yml".format(name)]),
+              env=env,
+              pty=True)
+
+    _fn.__doc__ = "Run the Ansible playbook at ansible/{}.yml".format(name)
+    return _fn
+
+
+playbooks = Collection("playbooks")
+
+for f in Path("ansible").glob("*.yml"):
+    t = task(
+        help={
+            "limit": "Limit hosts to an Ansible inventory pattern",
+            "tags": "Limit tasks to an Ansible tag pattern",
+            "user": "User to connect to the remote host as",
+        })(playbook_task_factory(f.stem))
+    playbooks.add_task(t, name=f.stem, default=(f.stem == "all"))
+
+ns = Collection(pb=playbooks)
