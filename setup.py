5de3b0d795a05351432f3218fd7425802954f41a
Jesse B. Hannah <jesse@jbhannah.net>
2019-11-06 16:30:54 -0700

    Start moving from Invoke to Click

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+
+def read_requirements():
+    with Path.cwd().joinpath("requirements.txt").open() as file:
+        return file.readlines()
+
+
+setup(name="hannah_family.infrastructure",
+      version="0.1.0",
+      description="Hannah Family IT infrastructure",
+      author="Jesse B. Hannah",
+      author_email="jesse@jbhannah.net",
+      url="https://github.com/hannnahs-family/infrastructure",
+      license="MIT",
+      install_requires=read_requirements(),
+      packages=find_packages("src"),
+      package_dir={"": "src"},
+      python_requires="== 3.7.*",
+      entry_points={
+          "console_scripts": "inf = hannah_family.infrastructure.cli:cli"
+      })
