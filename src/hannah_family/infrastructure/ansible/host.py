1e751d6a34b3ad7538eab81f947cc957c795614a
Jesse B. Hannah <jesse@jbhannah.net>
2019-11-09 23:37:49 -0700

    Ansible host variable reading

---
+++ src/hannah_family/infrastructure/ansible/host.py
@@ -0,0 +1,51 @@
+from json import dumps
+
+from ansible.playbook.play import Play
+from ansible.template import AnsibleJ2Template
+
+from .loader import Loader
+
+NONE_PLAY_DATA = {"name": "__None"}
+
+
+class InvalidHostNameError(Exception):
+    """Raised when the host name does not match an inventory host entry."""
+    pass
+
+
+class Host:
+    def __init__(self, hostname, source=None, loader=None):
+        self._loader = Loader() if loader is None else loader
+        host = self._loader.get_host(hostname, source)
+
+        if host is None:
+            raise InvalidHostNameError(hostname)
+
+        self._host = host
+        self._vars = {}
+
+    def get_variables(self, play_data={}):
+        if "name" not in play_data:
+            vars_key = dumps(play_data)
+        else:
+            vars_key = play_data["name"]
+
+        if vars_key not in self._vars:
+            self._vars[vars_key] = self._loader.get_vars(host=self._host,
+                                                         data=play_data)
+
+        return self._vars[vars_key]
+
+    def get_variable(self, name, play_data={}):
+        play_vars = self.get_variables(play_data)
+        var = self.get_variables(play_data).get(name)
+
+        if isinstance(var, dict):
+            return {key: self._render(val, play_data) for key, val in var}
+        elif isinstance(var, list):
+            return [self._render(val, play_data) for val in var]
+        else:
+            return self._render(var, play_data)
+
+    def _render(self, val, play_data):
+        return AnsibleJ2Template(val).render(self.get_variables(play_data))
---
+++ src/hannah_family/infrastructure/ansible/loader.py
@@ -0,0 +1,68 @@
+from pathlib import Path
+
+from ansible.config.manager import ConfigManager
+from ansible.inventory.manager import InventoryManager
+from ansible.parsing.dataloader import DataLoader
+from ansible.parsing.vault import get_file_vault_secret
+from ansible.playbook.play import Play
+from ansible.vars.manager import VariableManager
+
+
+class Loader:
+    def __init__(self):
+        self._config = ConfigManager()
+        self._loader = DataLoader()
+        self._file_vault_secrets = {}
+        self._inventory: InventoryManager = None
+        self._variables: VariableManager = None
+
+    def _load_file_vault_secrets(self, password_file_path=None):
+        if password_file_path is None:
+            password_file_path = self._config.get_config_value(
+                "DEFAULT_VAULT_PASSWORD_FILE")
+
+        password_file = Path(password_file_path)
+
+        if password_file.name in self._file_vault_secrets:
+            return
+
+        if not password_file.is_file():
+            raise FileNotFoundError(password_file)
+
+        self._file_vault_secrets[password_file.name] = get_file_vault_secret(
+            filename=password_file, loader=self._loader)
+        self._file_vault_secrets[password_file.name].load()
+        self._loader.set_vault_secrets([
+            (password_file.name, self._file_vault_secrets[password_file.name])
+        ])
+
+    def _get_inventory(self, source=None):
+        if source is None:
+            source = self._config.get_config_value("DEFAULT_HOST_LIST")
+
+        if self._inventory is None:
+            self._inventory = InventoryManager(loader=self._loader,
+                                               sources=source)
+        else:
+            sources = source if isinstance(source, list) else [source]
+            (self._inventory.parse_source(s) for s in sources)
+            self._inventory.reconcile_inventory()
+
+        return self._inventory
+
+    def _get_variables(self, source=None):
+        self._load_file_vault_secrets()
+        if self._variables is None:
+            self._variables = VariableManager(
+                loader=self._loader, inventory=self._get_inventory(source))
+
+        return self._variables
+
+    def get_host(self, hostname, source=None):
+        return self._get_inventory(source).get_host(hostname)
+
+    def get_vars(self, host, data={}):
+        play = Play.load(data,
+                         loader=self._loader,
+                         variable_manager=self._get_variables())
+        return self._get_variables().get_vars(host=host, play=play)
