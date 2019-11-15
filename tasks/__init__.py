79123bb4fd946ff9902b6429f1caeb1a040b0a7c
Jesse B. Hannah <jesse@jbhannah.net>
2019-11-15 15:57:13 -0700

    Remove Invoke

--- tasks/__init__.py
+++
@@ -1,8 +0,0 @@
-from invoke import Collection
-
-from .helm import helm
-from .playbooks import playbooks
-from .ssh import ssh
-from .vault import vault
-
-ns = Collection(helm=helm, pb=playbooks, ssh=ssh, vault=vault)
